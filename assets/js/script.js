839d742e1f137a3dc31bc1a01c4fc0d5ee6d7d1e
AbdullahHamadax <abdallahamada114@gmail.com>
2024-05-15 21:02:51 +0300

    Finished header and replaced images

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 /*
-Loading will end after the website is loaded.
+Loading will end after the website is loaded....
 */
 
 const preloader = document.querySelector("[data-preaload]");
@@ -9,4 +9,53 @@ const preloader = document.querySelector("[data-preaload]");
 window.addEventListener("load", function () {
   preloader.classList.add("loaded");
   this.document.body.classList.add("loaded");
+});
+
+/* adding event listener on multiple elements.. */
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+};
+
+/* NAVBAR */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+};
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
+
+/* HEADER */
+
+const header = document.querySelector("[data-header]");
+
+let lastScrollPos = 0;
+
+const hideHeader = function () {
+  const isScrollBottom = lastScrollPos < window.scrollY;
+  if (isScrollBottom) {
+    header.classList.add("hide");
+  } else {
+    header.classList.remove("hide");
+  }
+  lastScrollPos = window.scrollY; // to prevent the header tab from going hidden even when
+  // scrolling up xDDD
+};
+
+// This makes the header tab pop out with an animation, it's background is going to be a bit greyish too
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 50) {
+    header.classList.add("active");
+    hideHeader();
+  } else {
+    header.classList.remove("active");
+  }
 });
